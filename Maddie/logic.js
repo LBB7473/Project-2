0a0839614ef5416b574a35ffcc12fe027ef789f2
Madeline Sachtler <madelinegsachtler@gmail.com>
2021-04-24 10:51:38 -0700

    created html

---
+++ Maddie/logic.js
@@ -0,0 +1,15 @@
+var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 5
+  });
+  
+  // Add a tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
